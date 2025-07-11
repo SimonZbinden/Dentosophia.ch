a93f33dc09dd265ec98508043043a8f9ffae55d7
ZbindenSimon <sizbinden@ethz.ch>
2025-07-11 18:53:27 +0200

    save

--- python/python_modules.py
+++ python/python_modules.py
@@ -5,3 +5,7 @@ def import_config(file: Path):
     with open(file, "r") as f:
         config = yaml.safe_load(f)
     return config
+
+def load_config(page: str) -> dict:
+    with open(f"config/{page}.yaml") as f:
+        return yaml.safe_load(f)
